7b9120eee31c88ea3b82f52e2438ae5d2466d3c2
Kneir-E <116056222+Kneir-E@users.noreply.github.com>
2025-01-18 07:05:30 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './Card.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Card source='./assets/img.jpg' title='image_name' description='image_description'/>
+    </>
+  );
+}
+
+export default App
